b94b99d9823d8bcbb27448f06659b3f0e005c808
Halvor Eggen <halvore@gmail.com>
2025-06-02 16:09:57 +0200

    .

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: process.env.NODE_ENV === 'production' 
-        ? `https://${process.env.VERCEL_URL}/auth/google/callback`
+        ? "https://lottolista.vercel.app/auth/google/callback"
         : "/auth/google/callback"
 }, (accessToken, refreshToken, profile, done) => {
     const userEmail = profile.emails[0].value;
